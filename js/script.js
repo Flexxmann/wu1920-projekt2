cd899a1cd00771f1bf1569ef40a3c1d9791f3a08
Felix Brun <felix.brun@elev.ga.ntig.se>
2020-03-11 11:34:46 +0100

    a

---
+++ js/script.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {myfunction()};
+
+var navbar = document.getElementById("nav");
+
+var sticky = navbar.offsetTop;
+
+
+function myfunction() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("sticky")
+    } else  {
+        navbar.classList.remove("sticky");
+    }
+}
